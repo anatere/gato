a2892e6fe641ef282a93dfc368909a26bca8409b
anatere <trianovalenzuela@gmail.com>
2019-03-04 11:46:44 -0600

    Used of the fuction Sort

--- gato/src/index.js
+++ gato/src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import styled, {css}from 'styled-components'
 
 
+const Title = styled.h1`
+  font-size: 1.7em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const Input = styled.input.attrs(({ size }) => ({
+  margin: size || "1em",
+  padding: size || "1em"
+}))`
+  color:palevioletred ;
+  font-size: 1em;
+  border: 2px solid orange;
+  border-radius: 3px;
+`;
+
 class Game extends React.Component {
   constructor() {
     super();
@@ -17,12 +41,17 @@ class Game extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
 }
+
+
+
+
+
 handleComparativa({num1, num2, num3} ){
-  var  result, result2, result3
-        result= (num1>num3) ? num1 : num3;
-        result2 = (num2>num3) ? num2 : num3;
-        return result3 = (result<result2) ? result : result2;
+    var result= [num1,num2,num3]
+    result.sort (function(a,b) {return a-b;})
+    return result[1];
   }
+
   handleSubmit(e) {
    e.preventDefault();
    var resultadofinal = this.handleComparativa(this.state)
@@ -40,11 +69,14 @@ handleInputChange(e) {
 
       //console.log("el numero de en medio es--->",result3);
   render() {
+
     return (
+
     <div>
       <form onSubmit={this.handleSubmit}>
+    
       <h4> Primer valor </h4>
-      <input
+      <Input
         type="text"
         name="num1"
         value={this.state.num1}
@@ -53,7 +85,7 @@ handleInputChange(e) {
         />
 
       <h4> Segundo valor </h4>
-      <input
+      <Input
         type="text"
         name="num2"
         value={this.state.num2}
@@ -62,7 +94,7 @@ handleInputChange(e) {
         />
 
       <h4> Tercer valor </h4>
-      <input
+      <Input
       type="text"
       name="num3"
       value={this.state.num3}
@@ -70,13 +102,13 @@ handleInputChange(e) {
       placeholder="Ingrese numero"
       />
 
-      <button
+      <Button
       type="submit">
       Comparar
-      </button>
+      </Button>
 
       <h4> Resultado </h4>
-      <input
+      <Input
         type="text"
         name="result"
         value={this.state.result}
