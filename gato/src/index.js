22325750e50ef826954ed4a98137f0e277c3d513
anatere <trianovalenzuela@gmail.com>
2019-03-12 14:08:47 -0600

    Using the event keyUp

--- gato/src/index.js
+++ gato/src/index.js
@@ -14,40 +14,36 @@ class Game extends React.Component {
 
    handleComparativa(e){
 
+  console.log(e.keyCode);
 
 
-   console.log(e.keyCode);
-
-
-    // console.log("evento-->",e);
-    // console.log("target o input--->",e.target.value);
+    //console.log("evento-->",e);
+     console.log("target o input--->",e.target.value);
      var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q',
-      'R','S','T','U','V','W','X','Y','Z'];
+      'R','S','T','U','V','W','X','Y','Z',e.keyUp==26];
 
      abecedario.forEach((element, index)=> {
 
-
        if(e.target.value == element){
-         console.log("primer validacion-->");
+        console.log("primer validacion-->");
          if(index==26) {
            this.setState({
              result:abecedario[0]
            })
-          console.log("segunda validacion-->");
+        console.log("segunda validacion-->");
           console.log(abecedario[0]);
          return
        }
-
           this.setState({
             result:abecedario[index+1]
           });
           console.log(abecedario[index+1]);
      }
 
-     if (e.keyCode == 8){
-
-
-     }
+     // if (e.KeyUp == 8){
+     //
+     //
+     // }
 
      });
 
@@ -77,7 +73,7 @@ class Game extends React.Component {
 
     return (
       <div>
-      <textarea value={this.state.result} onKeyUp={this.handleComparativa} name="texto">  </textarea>
+      <textarea value={this.state.result} onChange={this.handleComparativa} name="texto">  </textarea>
       </div>
     );
   }
